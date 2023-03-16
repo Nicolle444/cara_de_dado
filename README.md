853c22e3a7c2e251b48e89f781e7435ae4f8d0dc
Nicolle444 <nicollecelis1@gmail.com>
2023-03-15 20:31:07 -0500

    se agrego un programa

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # cara_de_dado
 programa que al poner el numero de una cara, imprime el numero de la cara opuesta
+
+# Análisis
+
+# Diseño
+
+## Diagrama de flujo
+![Diagrama de flujo](diagrama.png "diagrama de flujo")
+
---
+++ cara_de_dado.py
@@ -0,0 +1,24 @@
+# cara opuesta del dado
+print("--------------------------------")
+print("---------cara del dado----------")
+print("--------------------------------")
+
+# input
+a = int(input("Ingrese un número del 1 al 6: "))
+
+
+# procesing and output
+if a == 1:
+    print("La cara opuesta es 6")
+elif a == 2:
+    print("La cara opuesta es 5")
+elif a == 3:
+    print("La cara opuesta es 4")
+elif a == 4:
+    print("La cara opuesta es 3")
+elif a == 5:
+    print("La cara opuesta es 2")
+else:
+    print("La cara opuesta es 1")
+
+
